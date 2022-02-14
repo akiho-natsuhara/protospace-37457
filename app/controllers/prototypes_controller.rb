c9f75b67dffd5c2569d8d00c72911de73baf7ede
akiho-natsuhara <natsu.050711@gmail.com>
2022-02-14 20:13:19 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,64 @@
+class PrototypesController < ApplicationController
+
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :move_to_index, only: [:edit, :update, :destroy]
+
+    def index
+      @prototypes = Prototype.all
+    end
+
+    def new
+      @prototype = Prototype.new
+    end
+
+    def create
+      @prototypes = Prototype.new(prototype_params)
+      if @prototypes.save
+        redirect_to root_path(@prototype)
+      else
+        render :new
+      end
+    end
+
+    def show
+      @prototype = Prototype.find(params[:id])
+      @comment = Comment.new
+      @comments = @prototype.comments.includes(:user)
+    end
+
+    def edit
+      @prototype = Prototype.find(params[:id])
+    end
+
+    def update
+         @prototype = Prototype.find(params[:id])
+      if @prototype.update(prototype_params)
+        redirect_to prototype_path
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      prototype = Prototype.find(params[:id])
+      prototype.destroy
+      redirect_to root_path
+    end
+
+
+
+    private
+
+    def prototype_params
+      params.require(:prototype).permit(:catch_copy, :concept, :title, :image).merge(user_id: current_user.id)
+    end
+
+   
+
+    def move_to_index
+      prototype = Prototype.find(params[:id])
+      unless current_user == prototype.user
+       redirect_to action: :index
+      end
+    end
+end
